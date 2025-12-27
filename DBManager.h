c824bffdf81b4a1a4684cfc232fafcd063a1f056
wyzr48 <2916373642@qq.com>
2025-12-27 21:29:16 +0800

    修改了收藏航班函数

--- DBManager.h
+++ DBManager.h
@@ -62,7 +62,7 @@ public:
     Q_INVOKABLE bool updateFlightStatus(const QString& Flight_id, int newststus);  // 更新航班状态
     Q_INVOKABLE bool deleteFlight(const QString& Flight_id);    // 删除航班
 
-    Q_INVOKABLE bool collectFlight(int userId, const QString& flightId, const QString& createTime);  // 收藏航班
+    Q_INVOKABLE bool collectFlight(int userId, const QString& flightId);  // 收藏航班
     Q_INVOKABLE bool cancelCollectFlight(int userId, const QString& flightId);  // 取消收藏航班
     Q_INVOKABLE QVariantList queryCollectedFlights(int userId);  // 查询用户收藏的所有航班
     Q_INVOKABLE QVariantList queryCollectedFlightByNum(int userId, const QString& Flight_id);  // 按航班号查询收藏航班
